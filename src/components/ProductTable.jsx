d9b82e7561f3ccb22538b41d8553bdfd0facb561
dipvijay212 <dipvijaypatil212@gmail.com>
2026-06-25 21:50:22 +0530

    new Ui

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -21,7 +21,7 @@ const ProductTable = ({ products, onDelete }) => {
             {products.length === 0 ? (
               <tr>
                 <td colSpan="3" className="px-6 py-8 text-center text-gray-500">
-                  No products found. Add some products or check your search query.
+                  no products found. Add some products or check your search query.
                 </td>
               </tr>
             ) : (
